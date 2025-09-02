39614d06551dbf2427882884eb85917cb5e7ad4f
screa <screamok2@gmail.com>
2025-09-02 19:52:04 +0200

    5

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-8&n697@7nh6n7ip%8#1@zk)9qr=(wsi-vh9k5x=@%f^4r85v&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "web"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
